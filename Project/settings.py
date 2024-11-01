e03968a3dea630c052b0c29a4ea6d8146e80e1db
romeofrancisco <romeofrancisco.works@gmail.com>
2024-11-01 22:03:10 +0800

    fix image path url

--- Project/settings.py
+++ Project/settings.py
@@ -114,7 +114,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 
 AUTH_USER_MODEL = 'user_auth.User'
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
